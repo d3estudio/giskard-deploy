7f544130823a6cfe5330c973046f766825fa93d0
Victor Gama <hey@vito.io>
2016-09-05 02:39:31 -0300

    Add deploy.list command

--- src/giskarddeploy.js
+++ src/giskarddeploy.js
@@ -1,4 +1,5 @@
 var Base = requireBaseModule();
+var tree = require('ascii-tree');
 
 var GiskardDeploy = function() {
     Base.call(this);
@@ -15,21 +16,74 @@ var GiskardDeploy = function() {
         commands: String
     });
 
-    var public,
-        private;
+    const generateTree = (arr) => {
+        let groups = {
+            name: 'Projects',
+            children: []
+        };
+        const generateRawTree = (child, level) => {
+            level = level || 1;
+            let input = '';
+            if(child.name !== '/') {
+                input = '#'.repeat(level) + child.name;
+            }
+            if(!child.children.length) {
+                input += ` (${child.path})`;
+            }
+            input += "\n" + child.children.map(c => generateRawTree(c, level + 1)).join('');
+            return input + "\n";
+        };
+        arr.sort()
+            .forEach(p => {
+                const components = p.split('/');
+                let target = groups;
+                for(let i = 0; i < components.length; i++) {
+                    const name = components[i];
+                    const t = target.children.find(x => x.name === name);
+                    if(t) {
+                        target = t;
+                        continue;
+                    }
+
+                    const newItem = {
+                        name,
+                        children: [],
+                        path: (() => {
+                            let parent = target;
+                            let tree = [];
+                            while(parent) {
+                                if(parent.name !== 'Projects') {
+                                    tree.push(parent.name);
+                                }
+                                parent = parent.parent;
+                            }
+                            return (tree.length ? tree.reverse().join('/') + '/' : '') + name;
+                        })()
+                    };
+                    target.children.push(newItem);
+                    target = newItem;
+                }
+            });
+        console.log(JSON.stringify(groups, null, 2));
+        return generateRawTree(groups);
+    }
 
     this.respond(/((?:configure suas chaves)|(?:deploy\.config\.keys))$/i, (response) => {
         response.sendTyping();
         response.getUser().then(u => {
             if (u.isRoot()) {
-                response.user.ask('Qual a chave *publica*? :old_key: ', this.Context.REGEX, /(.*)$/i)
+                var public,
+                    private;
+                response.user.ask('Qual a chave *pública*? :old_key: ', this.Context.REGEX, /(.*)$/im)
                     .then((answer) => {
+                        console.log(answer);
                         public = (answer.match[1] || '').replace(/`/g, '').trim();
                         if (!public.length) {
                             return answer.reply('Preciso que me informe a chave pública :disappointed:');
                         }
-                        answer.user.ask('Ok, e qual a chave *privada*? :old_key: ', this.Context.REGEX, /(.*)$/i)
+                        answer.user.ask('Ok, e qual a chave *privada*? :old_key: ', this.Context.REGEX, /(.*)$/im)
                             .then((answer) => {
+                                console.log(answer);
                                 private = (answer.match[1] || '').replace(/`/g, '').trim();
                                 if (!private.length) {
                                     return answer.reply('Preciso que me informe a chave privada, pode confiar em mim! :disappointed:');
@@ -73,12 +127,11 @@ var GiskardDeploy = function() {
         });
     });
 
-    var name,
-        user,
-        host,
-        commands;
-
     this.respond(/(?:(?:configure (?:um\s)?deploy(?:\spara)?(\s[^\s]+)?)|(?:deploy\.new(?:\s([^\s]+))?))$/i, (response) => {
+        var name,
+            user,
+            host,
+            commands;
         response.sendTyping();
         response.getUser().then(u => {
             if (u.isRoot()) {
@@ -150,7 +203,7 @@ var GiskardDeploy = function() {
                                         } else {
                                             answer.reply(`Pronto! Não esqueça de me dar uma chave privada para trabalhar nesse server com o comando \`configure suas chaves\` ou \`deploy.configure\` \`\`\``)
                                         }
-                                        Projects.update({}, {
+                                        Projects.update({name: name}, {
                                             name, user, host, commands
                                         }, {
                                             upsert: true
@@ -173,6 +226,21 @@ var GiskardDeploy = function() {
                 response.reply('Você não pode falar assim comigo :(');
             }
         });
+    });
+
+    this.respond(/(?:(?:liste (?:projetos de deploy|deploys configurados)|deploy\.list))/i, (response) => {
+        response.sendTyping();
+        Projects
+            .find()
+            .exec()
+            .then(arr => {
+                if(arr.length < 1) {
+                    return response.reply(':hushed: Parece que não existe nenhum projeto de deploy configurado.');
+                }
+                response.reply('Você pediu, aqui está. Eu mesmo desenhei, espero que goste. :robin:')
+                response.reply('>```' + tree.generate(generateTree(arr.map(p => p.name))) + '```');
+            })
+            .catch(e => console.error(e));
     });
 };
 
